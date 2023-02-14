b859e3c35e3621dfdfaeec0b7d2ae344cf1291f6
fromct <512799311@qq.com>
2023-02-14 23:44:38 +0800

    feat:init

---
+++ src/components/Count.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+let count = ref(1);
+function add() {
+  console.log("add");
+  count.value++;
+}
+</script>
+<template>
+  <div>
+    this is count=={{ count }}
+
+    <div><span @click="add">add</span></div>
+  </div>
+</template>
+<style scoped></style>
